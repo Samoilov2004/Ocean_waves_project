ae9f5019428ba4aa8b2fd20132eeaf875562e7b8
Mihail Samoilov <114419554+Samoilov2004@users.noreply.github.com>
2024-06-21 04:30:26 +0300

    Update beta.py

--- beta.py
+++ beta.py
@@ -8,14 +8,20 @@ import module as m
 
 #print(m.distance(37.61, 55.75, -99.13, 19.43))
 
+
 DATA = m.coord_data_like('/Users/mihail/Desktop/Ocean_waves_project/GRD/pacific4min.grd')
 
 
 print(np.shape(DATA))
-DATAN = m.distance_data(DATA, -20.54, 175.393)
+DATAN = m.distance_data(DATA, -60, 210)
 print(np.shape(DATAN))
 
 print(DATAN[0])
 
-im = plt.imshow(DATAN, cmap='bwr')
+
+
+fig, ax = plt.subplots(figsize=(12, 7)) 
+im = ax.imshow(DATAN, cmap='cool', extent = [m.give_description('/Users/mihail/Desktop/Ocean_waves_project/GRD/pacific4min.grd')[1][i] for i in [2, 3, 0, 1]])
+fig.colorbar(im, ax=ax)
+
 plt.show()
