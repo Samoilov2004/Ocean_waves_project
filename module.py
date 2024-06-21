c6af32d6338afa6828db5e72e7e77e74c3ebf763
Mihail Samoilov <114419554+Samoilov2004@users.noreply.github.com>
2024-06-21 03:08:24 +0300

    Update module.py

--- module.py
+++ module.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap as LSC
+from pyproj import Geod
 
 def cmap5():
     negative_colors = [(0.0, 'navy'), (0.5, 'skyblue'), (1.0, 'white')]
@@ -14,6 +13,7 @@ def cmap5():
     colors = np.vstack((negative_cmap(np.linspace(0, 1, 256)), positive_cmap(np.linspace(0, 1, 256))))
     my_cmap = LSC.from_list('my_cmap', colors)
     return my_cmap
+cmap5 = cmap5()
 
    
 def give_description(link):
@@ -43,7 +43,7 @@ def give_description(link):
     DATA = np.array(DATA).reshape(rows, cols)
     DATA = np.flip(DATA, axis=0) #дело в том что в файле строки снизу вверх идут и поэтому так пишем
     f.close()
-    return [DATA, [lon_min, lon_max, lat_min, lat_max]]
+    return [DATA, [lat_min, lat_max, lon_min, lon_max], [rows, cols]]
    
 
 give_data = lambda link: give_description(link)[0]
@@ -51,13 +51,36 @@ give_data = lambda link: give_description(link)[0]
 	
 def paint_map(link):
 	DATA = give_data(link)
-	# Создаем фигуру и ось
-	fig, ax = plt.subplots(figsize=(12, 7))  # Изменяем размер фигуры
-
-	# Рисуем карту с помощью imshow()
-	im = ax.imshow(DATA, cmap=cmap5, vmin=-max_dep, vmax=max_dep)
+	limits = [give_description(link)[1][i] for i in [2, 3, 0, 1]] #сначала прописывает долготу на осях, потом широту
+	max_dep = DATA.max()	
+	fig, ax = plt.subplots(figsize=(12, 7)) 
+	im = ax.imshow(DATA, cmap=cmap5, vmin=-max_dep, vmax=max_dep, extent = limits) #extent создает границы, а минимум и максимум для нормального отображения цветов
 
 	# Добавляем цветовую шкалу
 	fig.colorbar(im, ax=ax)
+	ax.set_ylabel('Latitude')
+	ax.set_xlabel('Longitude')
+	plt.show()
 	
-    
+
+def distance(lon1, lat1, lon2, lat2):
+    geod = Geod(ellps='WGS84')
+    point1 = (lon1, lat1)
+    point2 = (lon2, lat2)
+
+    azimuth1, azimuth2, distance = geod.inv(*point1, *point2)
+    return distance / 1000
+   
+   
+def coord_data_like(link):
+	DATA, limits, size = give_description(link)
+	lat_lims = np.linspace(limits[0], limits[1], size[0])
+	lon_lims = np.linspace(limits[2], limits[3], size[1])
+	DATA_NEW = [(i, q) for i in lat_lims for q in lon_lims]
+	DATA_NEW = np.array(DATA_NEW).reshape(size[0], size[1], 2)
+	return DATA_NEW
+	
+ 
+def distance_data(DATA, *point):
+	lon, lat = point
+	print(DATA[0][0])
